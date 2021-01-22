220ee5630de592c73d19dc480f0b1bf3aff86ec5
Laura Murillo <laura2172082@correo.uis.edu.co>
2021-01-22 15:48:25 -0500

    mi primer trabajo

--- diplomado.py
+++ diplomado.py
@@ -11,7 +11,17 @@ N = 101
 L = 400.0
 H = 50.0
 k = 1.0
+#busca mf6 en la carpeta especificada y guarda los archivos 
+sim = flopy.mf6.MFSimulation(sim_name=name, exe_name="C:\Users\lauri\Downloads\mf6.2.0\bin", version="mf6", sim_ws="workspace")
+ 
+#crea ibjetos de flopy TDIS
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)]
 
-sim = flopy.mf6.MFSimulation(
-    sim_name=name, exe_name="mf6", version="mf6", sim_ws="."
-)
+#crea el paquete de objetos flopy tms 
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+
+#crea el modelo del flujo de agua 
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
+
